b6a7cb113d57ff77b83f475943c21adde2d6f812
Tsubulko <pashatsubulko@gmail.com>
2023-02-10 20:59:32 +0300

    feat: pass lng

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+function drow(){
+  document.querySelector("body").innerHTML = `<div class="logo-container">
+  <h1>CREATE YOUR PASSWORD</h1>
+</div>
+<div class="creation-button"><p>CREATE!</p></div>
+<div class="pass-container">
+  <input type="text" value="" id="myInput">
+  <div class="tooltip">
+    <button class="copy-btn" onclick="copyText()" onmouseout="outFunc()">
+      <span class="tooltiptext" id="myTooltip">Copy to clipboard</span>
+      <span class="copy-btn-text">Copy text</span>
+      </button>
+    </div>
+</div>`
+}
+drow();
+
 document.querySelector(".creation-button").onclick = showPass;
 function randomInteger(max) {
   return Math.floor(Math.random() * max);
@@ -85,6 +102,7 @@ function copyText(){
   let tooltip = document.getElementById("myTooltip");
   tooltip.innerHTML = "Copied: " + copyText.value;
 }
+
 function outFunc() {
   let tooltip = document.getElementById("myTooltip");
   tooltip.innerHTML = "Copy to clipboard";
@@ -94,3 +112,19 @@ function showPass() {
   let lng = prompt('Enter password length', 8);
   document.querySelector("input").value = ` ${createPass(Number(lng))}`;
 }
+// function qwe(){
+//   onMouseOver=this.select();
+//   onMouseOut=this.value +=' '; 
+//   this.value = this.value.slice(0, -1);
+// }
+
+// document.querySelector("input") = qwe();
+// function clearSelection() {
+//       if(document.selection && document.selection.empty) {
+//           document.selection.empty();
+//       } else if(window.getSelection) {
+//           var sel = window.getSelection();
+//           sel.removeAllRanges();
+//       }
+//   }
+  
