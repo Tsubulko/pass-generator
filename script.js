2719a09440c5da24a39d09cd1524cef4d10015dc
Jaroslav Makeichyk <jaroslau1@gmail.com>
2023-02-10 15:58:22 +0300

    feat: add generetor function

---
+++ script.js
@@ -0,0 +1,79 @@
+function randomInteger(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function createPass(passLength) {
+  const CHAR = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  let result = [];
+  for (let i = 0; i < passLength; i++) {
+    result.push(CHAR[randomInteger(CHAR.length)]);
+  }
+  let password = result.join("");
+  console.log(password)
+  return password;
+}
+
+createPass(8);
